f12dc88bebcc07106a8f2ef59262d8aef7906929
yongseopK <kk0@kakao.com>
2023-08-11 15:05:02 +0900

    연습 : 필터된 연도 끌어오기

---
+++ src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './ExpenseFilter.css';
+
+const ExpenseFilter = ({ onFilterByYear }) => {
+
+  const dropdownChangeHandler = e => {
+    const selectedYear = e.target.value;
+
+    // seletedYear가 ExpenseList에서 사용할 수 있도록 올려보내기
+    onFilterByYear(selectedYear);
+  };
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select onChange={dropdownChangeHandler}>
+          <option value='2023'>2023</option>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseFilter;
--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import './ExpenseList.css';
+import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 
 const ExpenseList = ({ items }) => {
-    return (
-        <div className='expenses'>
-            <ExpenseItem
-                title={items[0].title}
-                price={items[0].price}
-                date={items[0].date}
-            />
-            <ExpenseItem
-                title={items[1].title}
-                price={items[1].price}
-                date={items[1].date}
-            />
-            <ExpenseItem
-                title={items[2].title}
-                price={items[2].price}
-                date={items[2].date}
-            />
-        </div>
-    );
+
+  const filterChangeHandler = (selectedYear) => {
+    console.log(`리스트에서 출력하는 것 :${selectedYear}`);
+  };
+
+  return (
+    <Card className='expenses'>
+      <ExpenseFilter onFilterByYear={filterChangeHandler} />
+      <ExpenseItem
+        title={items[0].title}
+        price={items[0].price}
+        date={items[0].date}
+      />
+      <ExpenseItem
+        title={items[1].title}
+        price={items[1].price}
+        date={items[1].date}
+      />
+      <ExpenseItem
+        title={items[2].title}
+        price={items[2].price}
+        date={items[2].date}
+      />
+    </Card>
+  );
 };
 
 export default ExpenseList;
