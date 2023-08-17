b4fe6888b2ba406a87750698b01a6be41063c1fb
yongseopK <kk0@kakao.com>
2023-08-17 11:26:44 +0900

    목표설정 APP - 동적 인라인 스타일 변경

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -5,8 +5,14 @@ import Button from '../UI/Button/Button.js';
 const CourseInput = ({ onAdd }) => {
 
   const [enteredText, setEnteredText] = useState('');
+  // 입력값 검증 상태변수
+  const [isValid, setIsValid] = useState(true);
 
   const textChangeHandler = e => {
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+
     // console.log(e.target.value);
     setEnteredText(e.target.value);
   };
@@ -14,6 +20,11 @@ const CourseInput = ({ onAdd }) => {
   const formSubmitHandler = e => {
     e.preventDefault();
     // console.log(enteredText);
+
+    if (enteredText.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
 
     onAdd(enteredText);
     setEnteredText('');
@@ -23,7 +34,14 @@ const CourseInput = ({ onAdd }) => {
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>나의 목표</label>
-        <input type="text" onChange={textChangeHandler} value={enteredText} />
+        <input
+          style={{
+            background: isValid ? 'transparent' : 'salmon',
+            borderColor: isValid ? 'black' : 'red'
+          }}
+          type="text"
+          onChange={textChangeHandler}
+          value={enteredText} />
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
