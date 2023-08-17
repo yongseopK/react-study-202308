fd399b40bbd5af387e25f255908c466a779f369c
yongseopK <kk0@kakao.com>
2023-08-17 11:16:32 +0900

    목표설정 APP - 기본 구성(추가기능, 삭제기능)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,59 @@ import CourseInput from './components/CourseGoals/CourseInput';
 import CourseList from './components/CourseGoals/CourseList';
 import './App.css';
 
+
+const DUMMY_DATA = [
+  {
+    id: 'g1',
+    text: '리액트 컴포넌트 스타일링 마스터하기',
+  },
+  {
+    id: 'g2',
+    text: 'UI프로그래밍 고수되기',
+  },
+];
+
 const App = () => {
+
+  const [goals, setGoals] = useState(DUMMY_DATA);
+
+  // input에게 전달할 함수
+  const addGoalHandler = text => {
+    // console.log('전달받은 텍스트 : ', text);
+
+    const newGoal = {
+      id: Math.random().toString(),
+      text: text,
+    };
+
+    // 상태변수(배열) 수정
+    // const updateGoals = [...goals, newGoal];
+    setGoals(prevGoals => [...prevGoals, newGoal]);
+  };
+
+  // 삭제 이벤트 핸들러 courseitem까지 내려보내야 함
+  const deleteGoalHandler = id => {
+    // console.log(id);
+    setGoals((prevGoals) => prevGoals.filter(goal => goal.id !== id));
+  };
+
+  // courseList 조건부 렌더링
+  let listContent = <p style={{
+    color: 'red',
+    fontSize: '2em',
+    textAlign: 'center',
+  }}>목표를 등록해주세요!!</p>;
+  if (goals.length > 0) {
+    listContent = <CourseList items={goals} onDelete={deleteGoalHandler} />;
+  }
 
   return (
     <div>
       <section id='goal-form'>
-        <CourseInput />
+        <CourseInput onAdd={addGoalHandler} />
       </section>
       <section id='goals'>
-        <CourseList />
+        {listContent}
       </section>
     </div>
   );
--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './CourseList.css';
+import CourseItem from './CourseItem';
 
-const CourseList = () => {
+const CourseList = ({ items, onDelete }) => {
   return (
-    <ul className='goal-list'></ul>
+    <ul className='goal-list'>
+      {items.map((item) => {
+        return <CourseItem key={item.id} item={item} onDelete={onDelete} />;
+      })}
+    </ul>
   );
 };
 
