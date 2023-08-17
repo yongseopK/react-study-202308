8b2293b71d6992923c72affd5aece3d8c71fbf23
yongseopK <kk0@kakao.com>
2023-08-17 10:14:01 +0900

    목표설정 APP 기본 템플릿

--- src/App.js
+++ src/App.js
@@ -1,51 +1,19 @@
 import React, { useState } from 'react';
-import ExpenseList from './components/Expenses/ExpenseList';
-import NewExpense from './components/NewExpense/NewExpense';
+import CourseInput from './components/CourseGoals/CourseInput';
+import CourseList from './components/CourseGoals/CourseList';
+import './App.css';
 
 const App = () => {
 
-  // 지출 항목 객체 배열
-  const expenses = [
-    {
-      id: 1,
-      title: '바나나',
-      price: 20000,
-      date: new Date(2023, 3 - 1, 23)
-    },
-    {
-      id: 2,
-      title: 'BBQ치킨',
-      price: 20200,
-      date: new Date(2022, 5 - 1, 21)
-    },
-    {
-      id: 3,
-      title: '도미노피자',
-      price: 35000,
-      date: new Date(2023, 7 - 1, 14)
-    },
-    {
-      id: 4,
-      title: '엽기떡볶이',
-      price: 17000,
-      date: new Date(2021, 3 - 1, 28)
-    },
-  ];
-
-  // 지출 객체배열을 상태변수로 관리
-  const [expenseList, setExpenseList] = useState(expenses);
-
-  // ExpenseForm에게 내려보낼 함수
-  const addExpenseHandler = (newExpense) => {
-    console.log(newExpense);
-    setExpenseList([...expenseList, newExpense]);
-  };
-
   return (
-    <>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <ExpenseList items={expenseList} />
-    </>
+    <div>
+      <section id='goal-form'>
+        <CourseInput />
+      </section>
+      <section id='goals'>
+        <CourseList />
+      </section>
+    </div>
   );
 };
 
